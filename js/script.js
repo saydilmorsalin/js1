76db1e3630ba5b23b8830d13cfdef0ae38e08183
Morsalin <71771752+saydilmorsalin@users.noreply.github.com>
2021-06-09 21:50:27 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
-window.alert("Hello")
-function changeText(){
-    document.getElementById("demo").innerHTML ="Text after change";
-}
-// changeText();
+$(document).ready(function(){
+    // alert("Hello!");
+    $('.btn0').click(function(){
+        $('.hide').hide();
+    })
+    $('.btn1').click(function(){
+        $('.hide').show();
+    })
+    $('.btn2').click(function(){
+        $('.hide').toggle();
+    })
+    $('.btn3').click(function(){
+        $('.show').fadeIn();
+    })
+    $('.btn4').click(function(){
+        $('.show').fadeOut();
+    })
+    $('.btn5').click(function(){
+        $('.show').fadeToggle();
+    })
+    // $('.btn6').click(function(){
+    //     $('.show').sliceToggle();
+    // })
+
+
+
+
+
+
+})
